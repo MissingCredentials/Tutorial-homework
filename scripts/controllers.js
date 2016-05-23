9f17c0467c65cad7a10a160f8ce80962b95824d5
MissingCredentials <Footprints@protonmail.com>
2016-05-23 09:33:08 -0400

    Upload files
    Uploading the homework files fore testing

---
+++ scripts/controllers.js
@@ -0,0 +1,149 @@
+'use strict';
+
+angular.module('confusionApp')    //module defined on app.js with square brackets
+
+        .controller('MenuController', ['$scope', 'menuFactory', function($scope, menuFactory) {
+
+         //gets dishes array from DB through menuFactory service
+            $scope.showMenu = false;
+            $scope.message = "Loading ...";
+            menuFactory.getDishes().query(
+              function(response) {              //successful response
+                 $scope.dishes = response;
+                 $scope.showMenu = true;
+              },
+              function(response) {              //error response
+                 $scope.message = "Error: "+response.status + " " + response.statusText;
+              }
+            );
+
+         //menu tab selectors
+            $scope.tab = 1;                 //sets default tab to 1
+            $scope.filtText = '';           //for tabs in menu
+
+            $scope.select = function(setTab) {
+                $scope.tab = setTab;
+
+                if (setTab === 2) {
+                    $scope.filtText = "appetizer";
+                }
+                else if (setTab === 3) {
+                    $scope.filtText = "mains";
+                }
+                else if (setTab === 4) {
+                    $scope.filtText = "dessert";
+                }
+                else {
+                    $scope.filtText = "";
+                }
+            };
+
+            $scope.isSelected = function (checkTab) {
+                return ($scope.tab === checkTab);   //isSelected is true if tab is selected
+            };
+
+         //menu item description visibility
+            $scope.showDetails = false;
+
+            $scope.toggleDetails = function() {
+                $scope.showDetails = !$scope.showDetails;
+            };
+        }])
+
+        .controller('ContactController', ['$scope', function($scope) {
+
+            $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
+
+            var channels = [{value:"tel", label:"Tel."}, {value:"Email",label:"Email"}];
+
+            $scope.channels = channels;
+            $scope.invalidChannelSelection = false;
+
+        }])
+
+        .controller('FeedbackController', ['$scope', function($scope) {
+
+            $scope.sendFeedback = function() {
+
+                console.log($scope.feedback);
+
+                if ($scope.feedback.agree && ($scope.feedback.mychannel === "")) {
+                    $scope.invalidChannelSelection = true;
+                    console.log('incorrect');
+                }
+                else {
+                    $scope.invalidChannelSelection = false;
+                    $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
+                    $scope.feedback.mychannel="";
+                    $scope.feedbackForm.$setPristine();
+                    console.log($scope.feedback);
+                }
+            };
+        }])
+
+        .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', function($scope, $stateParams, menuFactory) {
+
+          //get the dish detailed data from the server / JSON file
+            $scope.showDish = false;
+            $scope.message = "Loading ...";
+            $scope.dish = menuFactory.getDishes().get({id:parseInt($stateParams.id,10)})
+              .$promise.then(                     //need $promise.then for the get() method error handling
+                          function(response){
+                              $scope.dish = response;
+                              $scope.showDish = true;
+                          },
+                          function(response) {
+                              $scope.message = "Error: "+response.status + " " + response.statusText;
+                          }
+          );
+
+        }])
+
+        .controller('DishCommentController', ['$scope', 'menuFactory', function($scope, menuFactory) {
+
+            $scope.mycomment = {rating:5, comment:"", author:"", date:""};
+
+            $scope.submitComment = function () {
+
+                $scope.mycomment.date = new Date().toISOString();
+                console.log($scope.mycomment);
+                $scope.dish.comments.push($scope.mycomment);
+
+                menuFactory.getDishes().update({id:$scope.dish.id},$scope.dish);
+
+                $scope.commentForm.$setPristine();
+
+                $scope.mycomment = {rating:5, comment:"", author:"", date:""};
+            };
+        }])
+
+        .controller('IndexController', ['$scope', 'menuFactory', 'corporateFactory', function($scope, menuFactory, corporateFactory) {
+
+          //get data for the dish from the server
+            $scope.showDish = false;
+            $scope.message="Loading ...";
+            $scope.dish = menuFactory.getDishes().get({id:0})
+              .$promise.then(
+                function(response){
+                  $scope.dish = response;
+                  $scope.showDish = true;
+                },
+                function(response) {
+                  $scope.message = "Error: "+response.status + " " + response.statusText;
+                }
+          );
+
+          //get data for the promotion from the server
+              $scope.promotion= menuFactory.getPromotion(0);
+          //get data for the leaders from the server
+              $scope.leader = corporateFactory.getLeader(3);
+
+        }])
+
+        .controller('AboutController', ['$scope', 'corporateFactory', function($scope, corporateFactory) {
+
+          $scope.leaders = corporateFactory.getLeaders();
+
+        }])
+
+;
